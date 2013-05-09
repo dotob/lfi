677eddc9a31c3631359d68176f75ada537ec0692
dotob <sk@dotob.de>
2013-05-09 09:53:19 +0200

    use order.name for target dir, mkdir

--- app/workers/copy_order_worker.rb
+++ app/workers/copy_order_worker.rb
@@ -5,21 +5,25 @@ class CopyOrderWorker
 
   def perform(order_id, copy_target_path)
     order = Order.find(order_id)
-    Rails::logger.debug "CopyOrderWorker: start to copy order #{order_id} with #{order.order_items.count} items to #{copy_target_path}"
+    copy_to = File.join copy_target_path, order.name
+    FileUtils.mkdir_p copy_to
+    Rails::logger.debug "CopyOrderWorker: start to copy order >#{order_id}< with #{order.order_items.count} items to #{copy_to}"
+    copy_count = 0
     for order_item in order.order_items
       begin
         images = order_item.images
         if images.any?
           first_path = images.first.path
           if File.exists? first_path
-            Rails::logger.debug "CopyOrderWorker: copy image #{first_path}"
-            FileUtils.cp first_path, copy_target_path
+            Rails::logger.debug "CopyOrderWorker: copy image from #{first_path}"
+            FileUtils.cp first_path, copy_to
+            copy_count += 1
           end
         end
       rescue SystemCallError => autsch
         Rails::logger.debug "CopyOrderWorker: copy of #{first_path} failed with #{autsch}"
       end
     end
-    Rails::logger.debug "CopyOrderWorker: ended to copy order #{order_id} to #{copy_target_path}"
+    Rails::logger.debug "CopyOrderWorker: ended to copy #{copy_count} images of  order >#{order_id}< to #{copy_to}"
   end
 end
